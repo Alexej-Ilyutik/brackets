4e2b3b0dd43b1c51ee9caa214359edf7b0b7aa54
Alexej Ilyutik <alexej.ilyutik@gmail.com>
2022-02-03 16:32:24 +0300

    feat: add new solution

--- src/index.js
+++ src/index.js
@@ -1,26 +1,49 @@
 module.exports = function check(str, bracketsConfig) {
-    let brackets = bracketsConfig.join('').replace(/,/g, '');
-    let stack = [];
-for (let bracket of str) {
-        let bracketsIndex = brackets.indexOf(bracket)
+    //     let brackets = bracketsConfig.join('').replace(/,/g, '');
+    //     let stack = [];
+    // for (let bracket of str) {
+    //         let bracketsIndex = brackets.indexOf(bracket)
 
-        if (bracketsIndex % 2 === 0) {
-            
-            if (bracket === brackets[bracketsIndex + 1] && stack[stack.length - 1] === bracketsIndex){
-                stack.pop();
-            } else if (bracket === brackets[bracketsIndex + 1] && stack[stack.length - 1] !== bracketsIndex) {
-                stack.push(bracketsIndex)
-            }
-            else{
-                stack.push(bracketsIndex)
-            }
-        } 
-        else {
-            if (stack.pop() !== bracketsIndex-1){
-                return false;
-            }
-        }
-        
-    }
-    return stack.length === 0
-}
+    //         if (bracketsIndex % 2 === 0) {
+
+    //             if (bracket === brackets[bracketsIndex + 1] && stack[stack.length - 1] === bracketsIndex){
+    //                 stack.pop();
+    //             } else if (bracket === brackets[bracketsIndex + 1] && stack[stack.length - 1] !== bracketsIndex) {
+    //                 stack.push(bracketsIndex)
+    //             }
+    //             else{
+    //                 stack.push(bracketsIndex)
+    //             }
+    //         }
+    //         else {
+    //             if (stack.pop() !== bracketsIndex-1){
+    //                 return false;
+    //             }
+    //         }
+
+    //     }
+    //     return stack.length === 0
+   let stack = [];
+   for (let i = 0; i < str.length; i++) {
+       for (let j = 0; j < bracketsConfig.length; j++) {
+           if (str[i] == bracketsConfig[j][0]) {
+               if (
+                   stack[stack.length - 1] == bracketsConfig[j][0] &&
+                   bracketsConfig[j][0] == bracketsConfig[j][1]
+               ) {
+                   stack.pop();
+               } else {
+                   stack.push(str[i]);
+               }
+           } else if (str[i] == bracketsConfig[j][1]) {
+               if (stack.length == 0) {
+                   return false;
+               }
+               if (stack[stack.length - 1] == bracketsConfig[j][0]) {
+                   stack.pop();
+               }
+           }
+       }
+   }
+   return stack.length === 0;
+};
